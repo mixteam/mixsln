5d62a4512e023dc6b7ec175fced6406a6c6b2687
terrykingcha <terrykingcha@gmail.com>
2013-09-30 11:33:08 +0800

    修改replace的bug

--- dist/mixsln.debug.js
+++ dist/mixsln.debug.js
@@ -2831,6 +2831,7 @@ hooks.on('app:start', function(){
 		// 并且两个页面都不是默认页面（也就是说如果是默认页面，不管怎么改变hash都不再会进入startup和show）
 		if (!isSameState && !(isSamePage && state.isDefault)) {
 			lastPage && lastPage.hide(lastState); //如果有上一个页面，就先执行上一个页面的hide方法
+			page.el.setAttribute('data-fragment', state.fragment);
 
 			// 如果被替换的这个dom上的state和当前页面的state不同
 			// 且是不同页面（也就是，在有dom缓存的清空下，页面只有当被别的页面替换时才会被销毁（teardown））
@@ -2842,7 +2843,6 @@ hooks.on('app:start', function(){
 
 				pagecache[state.fragment] = {state:state, page:page};
 				page.el.innerHTML = '';
-				page.el.setAttribute('data-fragment', state.fragment);
 				page.startup(state);	// 新的page执行startup
 			}
 
